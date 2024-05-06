f8fa357df1980a0eaa2a69f9a76df406b4c80611
funso1234 <165808381+funso1234@users.noreply.github.com>
2024-05-06 11:00:33 +0100

    Add files via upload

---
+++ TriangleAstheriks.java
@@ -0,0 +1,28 @@
+public class TriangleAstheriks{
+public static void main(String[] args){
+
+
+for(int counter = 1; counter <= 5; counter++){
+       for(int i = 1; i <= counter; i++)
+System.out.print("*");
+
+
+System.out.println();
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+}
+
+
+
